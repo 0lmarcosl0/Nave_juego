204dcfa18267ec85c3e535ac961f2531152be5b8
0lmarcosl0 <cuentainsana12uwu@gmail.com>
2023-08-10 20:48:48 -0300

    main2

--- main.py
+++ main.py
@@ -1,44 +1,21 @@
 import tkinter as tk
-import pygame
-
-medida = (650, 650)
-azul = (0, 0, 255)
-
-var = False
+import game
 
 def cerrar_ventana():
-    ventana.destroy()
-
-def abrir_vent_go():
-    ventana.withdraw()
-    global var
-    var = True
-
-
-pygame.init()
-
-ven_go = pygame.display.set_mode(medida)
-pygame.display.set_caption("juego")
-
+	ventana.quit()
+	
+def iniciar_juego():
+	ventana.detroy()
+	game.Game()
 ventana = tk.Tk()
 ventana.title("menu")
-ventana.geometry("650x650")
-ventana.config(bg="black")
+ventana.geometry("650x420")
+ventana.config(bg = "black")
 
+boton_jugar = tk.Button(ventana,text = "jugar",command=iniciar_juego,height = 1,width = 6,font=("ariel",20))
+boton_jugar.place(x = 290, y = 110)
 
-boton_salir = tk.Button(ventana, text="salir", command=cerrar_ventana)
-boton_salir.pack()
-
-boton_jugar = tk.Button(ventana, text="jugar", command=abrir_vent_go)
-boton_jugar.pack()
+boton_salir= tk.Button(ventana,text = "salir",command=cerrar_ventana,height = 1,width = 6,font=("ariel",20))
+boton_salir.place(x = 290, y = 210)
 
 ventana.mainloop()
-
-while not var: 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            var = True
-
-pygame.quit()
-
-ventana.destroy()
